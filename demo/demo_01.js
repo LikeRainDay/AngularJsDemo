a004b766915c41f5ed7e55fec86876b935ca5822
unknown <>
2017-05-22 20:42:40 +0800

    Initial commit

---
+++ demo/demo_01.js
@@ -0,0 +1,21 @@
+/**
+ * Created by housh on 2017/5/22.
+ */
+function phoneListCtl($scope) {
+    $scope.phones = [
+        {
+            "name": "Nexus S",
+            "snippt": "Fast just got faster with Nexus S."
+        },
+        {
+            "name":"Motorola XOOM with Wi-Fi",
+            "snippt":"The Next ,Next Generation tablet."
+        },
+        {
+            "name":"MOTOROLA XOOM",
+            "snippt":"The Next ,Next Generation tablet."
+        }
+    ];
+    $scope.hello="Hello , World!"
+
+}
